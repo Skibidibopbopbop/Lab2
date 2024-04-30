4ba78e47bbbad3ed3c5c4e3a9ca5278af64a5926
NICHOLAS <nicholasyeojl.23@ichat.sp.edu.sg>
2024-04-30 13:52:36 +0800

    Lab3_Changes

--- bmi.py
+++ bmi.py
@@ -5,10 +5,13 @@ def calculate_bmi(height, weight):
     print("BMI = " +str(calculate_bmi))
     if calculate_bmi < 18.5:
         print("You are Under Weight!")
+        return -1
     elif 18.5 <= calculate_bmi <= 25.0:
         print("You are normal weight")
+        return 1
     else:
         print("You are FATTTTT!!!")
+        return +1
 
 
  
